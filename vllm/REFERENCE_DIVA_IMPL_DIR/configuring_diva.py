e304bee8829349c8fba5570c36667d32df5534d3
Helw150 <wbh230@nyu.edu>
2025-05-23 17:17:57 -0400

    Reference DiVA Impl

---
+++ vllm/REFERENCE_DIVA_IMPL_DIR/configuring_diva.py
@@ -0,0 +1,11 @@
+from transformers import PretrainedConfig
+
+
+class DiVAConfig(PretrainedConfig):
+    model_type = "diva"
+
+    def __init__(
+        self,
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
